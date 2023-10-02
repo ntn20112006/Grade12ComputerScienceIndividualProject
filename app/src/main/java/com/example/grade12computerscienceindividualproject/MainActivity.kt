32475e86e49d9abc1f35fb3c3f79dff10bfa9e96
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2023-10-02 13:41:53 -0400

    Build Project

---
+++ app/src/main/java/com/example/grade12computerscienceindividualproject/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.grade12computerscienceindividualproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Grade 12 Computer Science Individual Project"
+include(":app")
