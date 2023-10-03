b008f8da03f6b647c37a4a6fec2bf293eb32784c
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2023-10-03 10:14:59 -0400

    Add presentation and repository folders and move files inside

--- app/src/main/java/com/example/grade12computerscienceindividualproject/presentation/Theme.kt
+++ app/src/main/java/com/example/grade12computerscienceindividualproject/presentation/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.grade12computerscienceindividualproject.ui.theme
+package com.example.grade12computerscienceindividualproject.presentation
 
 import android.app.Activity
 import android.os.Build
