4808d7fcf6d513f5d7b211760b9f592a0396de1b
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2023-10-05 10:03:52 -0400

    Add documentation for recipe list screen view model

--- app/src/main/java/com/example/grade12computerscienceindividualproject/presentation/ui/recipeList/RecipeListScreenViewModel.kt
+++ app/src/main/java/com/example/grade12computerscienceindividualproject/presentation/ui/recipeList/RecipeListScreenViewModel.kt
@@ -5,9 +5,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.grade12computerscienceindividualproject.domain.model.Recipe
 
+/**
+ * A recipe list screen fragment
+ *
+ * @property recipeList
+ */
 class RecipeListScreenViewModel : ViewModel() {
     private val recipeList: MutableState<List<Recipe>> = mutableStateOf(ArrayList())
 
+    /**
+     * Filter the recipeList based on recipe name
+     */
     fun filterRecipeList (recipeName: String) {
         // TODO Filter recipe list
     }
