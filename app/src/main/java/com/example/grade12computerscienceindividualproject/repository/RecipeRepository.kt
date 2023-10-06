f965dddbe3aee938cf35b0deb1fdcdc72e3e46ac
Tri Nghia Nguyen <tringhianguyen@tris-air.lan>
2023-10-05 20:20:48 -0400

    Add recipe repository write function

--- app/src/main/java/com/example/grade12computerscienceindividualproject/repository/RecipeRepository.kt
+++ app/src/main/java/com/example/grade12computerscienceindividualproject/repository/RecipeRepository.kt
@@ -1,8 +1,12 @@
 package com.example.grade12computerscienceindividualproject.repository
 
 import android.content.Context
+import android.util.Log
 import com.example.grade12computerscienceindividualproject.domain.model.Recipe
 import com.google.gson.Gson
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.File
 
 /**
  * A class to interact with external data
@@ -10,7 +14,7 @@ import com.google.gson.Gson
 class RecipeRepository {
     companion object {
         /**
-         * Read and a recipe json file
+         * Read a recipe json file
          * @return the recipe object
          */
         fun getRecipeFromJSON(path: String, context: Context): Recipe {
@@ -19,6 +23,24 @@ class RecipeRepository {
             }
             val gson = Gson()
             return gson.fromJson(file, Recipe::class.java)
+        }
+
+        /**
+         * Write a recipe json file
+         */
+        fun writeNewJSONRecipe(fileName: String, recipe: Recipe) {
+            try {
+                val file = File("app/src/main/assets/$fileName")
+                if (file.exists()) {
+                    // TODO Show that file already exist
+                    return
+                }
+                file.createNewFile()
+                File("app/src/main/assets/$fileName").writeText(Json.encodeToString(recipe))
+            } catch (e: Exception) {
+                // TODO Show user that something is wrong
+                Log.i("Error", e.message.toString())
+            }
         }
     }
 }
