59cf4c8bddeae372ba79ba3cfd2b61b5a4e44ae6
Tri Nghia Nguyen <tringhianguyen@tris-air.lan>
2023-11-08 15:10:26 -0500

    Add documentation for ModifyingRecipeInformationForUI file

--- app/src/main/java/com/example/grade12computerscienceindividualproject/utils/ModifyRecipeInformationForUI.kt
+++ app/src/main/java/com/example/grade12computerscienceindividualproject/utils/ModifyRecipeInformationForUI.kt
@@ -1,6 +1,16 @@
 package com.example.grade12computerscienceindividualproject.utils
 
+/**
+ * A file specializes in modifying the information of a recipe object
+ */
 class ModifyRecipeInformationForUI {
+    /**
+     * Calculate and return the average cook time per recipe yield
+     *
+     * @param time the cook time for the recipe yield
+     * @param yield the yield of the recipe
+     * @return the average cook time per recipe yield
+     */
     fun getAverageCookTimePerRecipeYield(time: List<String>, yield: Int) : List<String> {
         var minutes: Int = time[2].toInt()
         minutes += time[0].toInt() * 24 * 60
@@ -14,6 +24,12 @@ class ModifyRecipeInformationForUI {
         return listOf(days.toString(), hours.toString(), minutes.toString())
     }
 
+    /**
+     * Convert from time in encoded string into a list of days, hours, and minutes
+     *
+     * @param time the encoded time in string
+     * @return time in the form of a list of days, hours, and minutes
+     */
     fun getRecipeCookTime(time: String) : List<String> {
         val tempTimeList = mutableListOf<String>()
         var tempTime = ""
