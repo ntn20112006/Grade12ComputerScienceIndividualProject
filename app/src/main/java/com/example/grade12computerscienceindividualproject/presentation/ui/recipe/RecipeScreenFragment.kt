1c9cf7cd8d25e95c02f25dd237f4949fdf38630d
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2023-11-20 13:42:11 -0500

    Add recipe fragment and view model basic structure files

---
+++ app/src/main/java/com/example/grade12computerscienceindividualproject/presentation/ui/recipe/RecipeScreenFragment.kt
@@ -0,0 +1,28 @@
+package com.example.grade12computerscienceindividualproject.presentation.ui.recipe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+private lateinit var viewModel: RecipeScreenViewModel
+
+fun getRecipeScreenViewModel() : RecipeScreenViewModel {
+    return viewModel
+}
+
+class RecipeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+
+            }
+        }
+    }
+}
