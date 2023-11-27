8fdd266978375f3e4502e87ace8ca5a633e6b0cd
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2023-11-27 13:35:58 -0500

    Add UI for basic information about the recipe

--- app/src/main/java/com/example/grade12computerscienceindividualproject/presentation/ui/recipe/RecipeScreenViewModel.kt
+++ app/src/main/java/com/example/grade12computerscienceindividualproject/presentation/ui/recipe/RecipeScreenViewModel.kt
@@ -1,9 +1,66 @@
 package com.example.grade12computerscienceindividualproject.presentation.ui.recipe
 
+import android.os.Bundle
 import androidx.lifecycle.ViewModel
+import com.example.grade12computerscienceindividualproject.domain.model.Recipe
+import com.example.grade12computerscienceindividualproject.presentation.ui.recipeList.getRecipeListScreenViewModel
+import com.example.grade12computerscienceindividualproject.utils.ModifyRecipeInformationForUI
+import com.example.grade12computerscienceindividualproject.utils.RecipeListFilter
+import java.lang.StringBuilder
+import java.math.BigDecimal
+import java.math.RoundingMode
 
-class RecipeScreenViewModel () : ViewModel() {
+class RecipeScreenViewModel (
+    argument: Bundle
+) : ViewModel() {
+    val recipe: Recipe
+
     init {
+        recipe = RecipeListFilter.filterRecipeListByName(
+            argument.getString("RecipeName")!!,
+            getRecipeListScreenViewModel().recipeList.value)[0]
+    }
 
+    fun getRecipeScreenUITitleText(): String {
+        return recipe[1].name
+    }
+
+    fun getRecipeScreenUISubTitleText(): String {
+        val authors = StringBuilder()
+        authors.append("By ")
+        for (author in recipe[1].author) {
+            authors.append(author.name + ",")
+        }
+        val publishedDate = "Published on: " + recipe[1].datePublished.subSequence(0, 10)
+        return authors.subSequence(0, authors.length - 1).toString() + "\n" +
+                publishedDate
+    }
+
+    fun getRecipeScreenUIBodyText(): String {
+        val rating = "Rating: " + BigDecimal(recipe[1].aggregateRating.ratingValue)
+            .setScale(2, RoundingMode.HALF_UP).toString() +
+                " (" + recipe[1].aggregateRating.ratingCount + " ratings)"
+        val description = "Description:\n\t\t" + recipe[1].description
+        var cookTimeArray: List<String>? = null
+        if (recipe[1].totalTime != null)
+            cookTimeArray = ModifyRecipeInformationForUI.getRecipeCookTime(recipe[1].totalTime.toString())
+        var cookTime = "Total cook time: Unknown"
+        if (cookTimeArray != null) {
+            cookTime = "Total cook time required: " +
+                cookTimeArray[0] + " days, " +
+                cookTimeArray[1] + " hours, " +
+                cookTimeArray[2] + " minutes"
+        }
+        val nutrition = "Nutritions:" +
+                "\n\tCalories: " + recipe[1].nutrition.calories +
+                "\n\tCarbohydrates: " + recipe[1].nutrition.carbohydrateContent +
+                "\n\tFat: " + recipe[1].nutrition.fatContent +
+                "\n\tSaturated fat: " + recipe[1].nutrition.saturatedFatContent +
+                "\n\tCholesterol content: " + recipe[1].nutrition.cholesterolContent +
+                "\n\tProtein: " + recipe[1].nutrition.proteinContent +
+                "\n\tFiber: " + recipe[1].nutrition.fiberContent +
+                "\n\tSodium: " + recipe[1].nutrition.sodiumContent +
+                "\n\tSugar: " + recipe[1].nutrition.sugarContent
+        return rating + "\n" + cookTime + "\n\n" + description + "\n\n" + nutrition
     }
 }
--- app/src/main/java/com/example/grade12computerscienceindividualproject/utils/ModifyRecipeInformationForUI.kt
+++ app/src/main/java/com/example/grade12computerscienceindividualproject/utils/ModifyRecipeInformationForUI.kt
@@ -4,45 +4,49 @@ package com.example.grade12computerscienceindividualproject.utils
  * A file specializes in modifying the information of a recipe object
  */
 class ModifyRecipeInformationForUI {
-    /**
-     * Calculate and return the average cook time per recipe yield
-     *
-     * @param time the cook time for the recipe yield
-     * @param yield the yield of the recipe
-     * @return the average cook time per recipe yield
-     */
-    fun getAverageCookTimePerRecipeYield(time: List<String>, yield: Int) : List<String> {
-        var minutes: Int = time[2].toInt()
-        minutes += time[0].toInt() * 24 * 60
-        minutes += time[1].toInt() * 60
+    companion object {
+        /**
+         * Calculate and return the average cook time per recipe yield
+         *
+         * @param time the cook time for the recipe yield
+         * @param yield the yield of the recipe
+         * @return the average cook time per recipe yield
+         */
+        fun getAverageCookTimePerRecipeYield(time: List<String>, yield: Int): List<String> {
+            var minutes: Int = time[2].toInt()
+            minutes += time[0].toInt() * 24 * 60
+            minutes += time[1].toInt() * 60
 
-        minutes /= yield
-        val days: Int = minutes / 60 / 24
-        val hours: Int = minutes % (60 * 24) / 60
-        minutes %= 60
+            minutes /= yield
+            val days: Int = minutes / 60 / 24
+            val hours: Int = minutes % (60 * 24) / 60
+            minutes %= 60
 
-        return listOf(days.toString(), hours.toString(), minutes.toString())
-    }
-
-    /**
-     * Convert from time in encoded string into a list of days, hours, and minutes
-     *
-     * @param time the encoded time in string
-     * @return time in the form of a list of days, hours, and minutes
-     */
-    fun getRecipeCookTime(time: String) : List<String> {
-        val tempTimeList = mutableListOf<String>()
-        var tempTime = ""
-        for (character in time) {
-            if (character == 'P' || character == 'T')
-                continue
-            if (character == 'D' || character == 'H' || character == 'M') {
-                tempTimeList.add(tempTime)
-                tempTime = ""
-                continue
-            }
-            tempTime += character
+            return listOf(days.toString(), hours.toString(), minutes.toString())
         }
-        return tempTimeList
+
+        /**
+         * Convert from time in encoded string into a list of days, hours, and minutes
+         *
+         * @param time the encoded time in string
+         * @return time in the form of a list of days, hours, and minutes
+         */
+        fun getRecipeCookTime(time: String): List<String>? {
+            if (time.compareTo("null") == 0)
+                return null
+            val tempTimeList = mutableListOf<String>()
+            var tempTime = ""
+            for (character in time) {
+                if (character == 'P' || character == 'T')
+                    continue
+                if (character == 'D' || character == 'H' || character == 'M') {
+                    tempTimeList.add(tempTime)
+                    tempTime = ""
+                    continue
+                }
+                tempTime += character
+            }
+            return tempTimeList
+        }
     }
 }
