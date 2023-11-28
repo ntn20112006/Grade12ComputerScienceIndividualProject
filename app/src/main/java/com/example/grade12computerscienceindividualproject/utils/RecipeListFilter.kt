954b931138db2af943d7a6d84492d7cc085fd2d7
Tri Nghia Nguyen <tringhianguyen@Tris-MacBook-Air.local>
2023-11-27 19:04:40 -0500

    Rebase code by adding back the search recipe by name function

--- app/src/main/java/com/example/grade12computerscienceindividualproject/utils/RecipeListFilter.kt
+++ app/src/main/java/com/example/grade12computerscienceindividualproject/utils/RecipeListFilter.kt
@@ -23,5 +23,23 @@ class RecipeListFilter {
             }
             return tempRecipeList
         }
+
+        fun searchRecipeByName(recipeList: List<Recipe>, recipeName: String): Recipe {
+            return binarySearch(recipeList, recipeName, 0, recipeList.size - 1)
+        }
+
+        private fun binarySearch(input: List<Recipe>, searchItem: String, low:Int, high:Int): Recipe {
+            while(low <= high) {
+                val mid = (low + high) /2
+                when {
+                    searchItem > input[mid][1].name ->
+                        return binarySearch(input, searchItem, mid + 1, high)   // element is greater than middle element of array, so it will be in right half. Recursion will call the right half again
+                    searchItem < input[mid][1].name ->
+                        return binarySearch(input, searchItem, low, mid - 1)    //element is less than middle element of array, so it will be in left half of the array. Recursion will call the left half again.
+                    searchItem == input[mid][1].name -> return input[mid] // element found.
+                }
+            }
+            return input[0]
+        }
     }
 }
